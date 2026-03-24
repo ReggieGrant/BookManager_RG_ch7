778f1d819fa014d38d94dc14edd57b595a2085b6
ReggieGrant <grantreggie09@gmail.com>
2026-03-24 10:54:06 -0700

    Initial Commit

---
+++ BookManager_RG_ch7/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BookManager_RG_ch7
+//
+//  Created by Reginald Grant on 3/24/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BookManager_RG_ch7Tests/BookManager_RG_ch7Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BookManager_RG_ch7Tests.swift
+//  BookManager_RG_ch7Tests
+//
+//  Created by Reginald Grant on 3/24/26.
+//
+
+import Testing
+@testable import BookManager_RG_ch7
+
+struct BookManager_RG_ch7Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
